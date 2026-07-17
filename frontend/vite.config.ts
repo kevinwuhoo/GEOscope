83bfd7cb3eee7f90977794a995410a51eb223ee4
kevin <me@kevinformatics.com>
2026-07-17 11:05:00 -0700

    fix: prevent font swap

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,17 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
 
+const preventFontSwap = {
+  name: "prevent-font-swap",
+  transform(code: string, id: string) {
+    return id.includes("@fontsource") && id.includes(".css")
+      ? code.replaceAll("font-display: swap", "font-display: optional")
+      : undefined;
+  },
+};
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [preventFontSwap, react()],
   server: {
     proxy: {
       "/api": "http://127.0.0.1:8000",
